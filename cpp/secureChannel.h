9ac787db807d9aef8b497fce220369f7c0100ef4
edgchess <edgchess@aol.com>
2016-04-23 15:39:08 -0400

    Fixed defintion

--- cpp/secureChannel.h
+++ cpp/secureChannel.h
@@ -1,8 +1,8 @@
 
-class secureChannel {
+class SecureChannel {
 	public:
-		virtual ssize_t send(int sockfd, const void *buf, size_t len, int flags);
-		virtual ssize_t recv(int sockfd, void *buf, size_t len, int flags);
+		virtual ssize_t send(const void *buf, size_t len, int flags);
+		virtual ssize_t recv(void *buf, size_t len, int flags);
 		virtual char* encrypt(char* enc, const char* msg, const char* key, unsigned long long size);
 		virtual char* decrypt(char* msg, const char* enc, const char* key, unsigned long long size);
 		virtual char* mac(char* mac, const char* msg, const char* key, unsigned long long size);
