d23389492c3d76470e00ccba6eb92116f1702a22
celic <cceli@codequarry.net>
2016-04-24 03:21:48 -0400

    Big changes, simplifies all file structures

--- cpp/users/alice.cpp
+++ cpp/users/alice.cpp
@@ -1,25 +1,107 @@
-#include <stdio>
+#include <cstdlib>			// atoi
+#include <sys/socket.h>		// socket
+#include <arpa/inet.h>		// inet
+#include <stdio.h>			// printf
+#include <unistd.h>			// read
+#include <string.h>			// string and bzero
 
-#include "alice.h"
-#include "../channel/channel_wrapper.h"
-#include "../lib/utils.h"
+#include "../lib/utils.h"	// crypto dependencies
 
-int main(){
+#define MAX_MSG_SIZE 4096
+
+int main(int argc, char* argv[]){
+
+	// Check parameters
+	if(argc != 2){
+		printf("Error: Need port\n");
+		return -1;	
+	} 
 
 	// Initialize variables
-	int port = 9999;
+	int port = atoi(argv[1]);
+	char read_buffer[MAX_MSG_SIZE+1];
+	std::string write_buffer;
+	int return_call = -1;
+	int socket_fd, new_socket_fd;
+	struct sockaddr_in server, client;
+	socklen_t client_length;
 
-	// Host channel
-	ChannelWrapper channel;
+	printf("Binding to port %d\n", port);
 
-	if(channel.start_server(port)){
-	  	while (1){
-	    	
-	    	// Someone has connected 
-	  	}
-	}else{
-		printf("Error starting server on port %d\n", port);
+	// Open port
+	// Declare socket file descripter
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	// Validity check
+	if (socket_fd < 0){
+		printf("Error creating socket\n");
+	    return -1;
 	}
+	
+	// Initialize socket
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+
+    // Bind to port and listen for one connection (Bob)
+    if(bind(socket_fd, (sockaddr *)&server, sizeof(server)) == 0){
+      	listen(socket_fd, 1);
+    }
+
+    printf("Port bound. Waiting for Bob...\n");
+
+    // Bind client when it arrives
+    client_length = sizeof(client);
+    new_socket_fd = accept(socket_fd, (struct sockaddr*) &client, &client_length);
+
+    // Validity check
+    if(new_socket_fd < 0){
+    	printf("Error accepting client\n");
+    	return -1;
+    }
+
+    printf("Bob connected. Beginning key exchange\n");
+
+    // Begin key agreement
+    // Read Bob's half of the key (encrypted)
+    bzero(read_buffer, MAX_MSG_SIZE+1);
+    return_call = recv(new_socket_fd, read_buffer, MAX_MSG_SIZE, 0);
+    if(return_call < 0){
+    	printf("Error reading Bob's encrypted key\n");
+    	return -1;
+    }
+
+    // Determine end of message and store in string
+    read_buffer[return_call] = 0;
+    std::string encrypted_bob_key_half(read_buffer);
+    encrypted_bob_key_half = hexstr2str(encrypted_bob_key_half);
+
+    // Determine Bob's half of the key
+	std::string bob_key_half = decryptWithBothKeys("bob.pub", "alice.key", encrypted_bob_key_half);
+	
+	printf("Determined Bob's half of key. Sending Alice's half\n");
+
+	// Generate 128-bit random key for AES and encrypt with RSA
+	std::string alice_key_half = readRand(16);
+	std::string encrypted_alice_key = encryptWithBothKeys("bob.pub", "alice.key", alice_key_half);
+
+	// Send key half (encrypted) to Bob
+	write_buffer = str2hexstr(encrypted_alice_key);
+	if(send(new_socket_fd, write_buffer.c_str(), write_buffer.length(), 0) < 0){
+		printf("Error sending encrypted key\n");
+		return -1;
+	}
+
+	printf("Key agreed\n");
+
+	// Determine session key and split it into aes_key, aes_iv, hmac_password, and init_nonce
+	std::string session_key = session_hash(bob_key_half, alice_key_half);
+	std::string aes_key = hexstr2str(session_key.substr(0,32));
+	std::string aes_iv = hexstr2str(session_key.substr(32,32));
+	std::string hmac_password = session_key.substr(64,32);
+	std::string init_nonce = session_key.substr(96,32);
+
+    printf("Here is the nonce: %s\n", init_nonce.c_str());
 
 	return 0;
 }
--- cpp/users/bob.cpp
+++ cpp/users/bob.cpp
@@ -1,40 +1,98 @@
-#include "bob.h"
+#include <cstdlib>			// atoi
+#include <sys/socket.h>		// socket
+#include <arpa/inet.h>		// inet
+#include <stdio.h>			// printf
+#include <unistd.h>			// read
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <netdb.h> 
+#include <string.h>			// string and bzero
 
-#include <stdio>
+#include "../lib/utils.h"	// crypto dependencies
 
-#include "../channel/channel_wrapper.h"
-#include "../lib/utils.h"
+#define MAX_MSG_SIZE 4096
 
-int main(){
+int main(int argc, char* argv[]){
+
+	// Check parameters
+	if(argc != 2){
+		printf("Error: Need port\n");
+		return -1;	
+	} 
 
 	// Initialize variables
-	int port = 9999;
+	int port = atoi(argv[1]);
+	char read_buffer[MAX_MSG_SIZE+1];
+	std::string write_buffer;
+	int return_call = -1;
+	int socket_fd;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-	// Host channel
-	ChannelWrapper channel;
+	printf("Binding to port %d\n", port);
 
-	if(channel.connect_to_server(port)){
+	// Open port
+	// Declare socket file descripter
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-		// TODO FIX THIS FOR BOB INSTEAD OF ALICE
-		// Determine keys for each party
-		// std::string alice_key = readRand(16);
-		// std::string encrypted_alice_key = encryptWithBothKeys("bob.pub", "alice.key", alice_key);
-		// server.send_data(str2hexstr(encrypted_alice_key));
-
-		// std::string encrypted_bob_key = hexstr2str(server.recv_data());
-		// std::string bob_key = decryptWithBothKeys("bob.pub", "alice.key", encrypted_bob_key);
-
-		// std::string session_key = session_hash(alice_key, bob_key);
-		// std::string aes_key = hexstr2str(session_key.substr(0,32));
-		// std::string aes_iv = hexstr2str(session_key.substr(32,32));
-		// std::string hmac_password = session_key.substr(64,32);
-		// std::string init_nonce = session_key.substr(96,32);
-
-		// server.set_keys(aes_key, aes_iv, hmac_password, init_nonce);
-	}else{
-		printf("Unable to connect to port %d\n", port);
+	// Validity check
+	if (socket_fd < 0){
+		printf("Error creating socket\n");
+	    return -1;
 	}
 
+	// Initialize socket
+	server = gethostbyname("127.0.0.1");
+	serv_addr.sin_family = AF_INET;
+	bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(port);
+
+	// Bind to port
+	if(connect(socket_fd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0){
+		printf("Error connecting to socket\n");
+		return -1;
+	}
+
+	printf("Connected to Alice. Beginning key exchange\n");
+
+	// Begin key exchange
+	// Generate Bob's half of the key and encrypt it
+	std::string bob_key_half = readRand(16);
+	std::string encrypted_bob_key_half = encryptWithBothKeys("alice.pub", "bob.key", bob_key_half);
+
+	// Send encrypted key to Alice
+	if(send(socket_fd, encrypted_bob_key_half.c_str(), encrypted_bob_key_half.length(), 0) < 0){
+		printf("Error sending encrypted key\n");
+		return -1;
+	}
+
+	printf("Sent Bob's half of key to Alice.\n");
+
+	// Receive Alice's encrypted key
+	return_call = recv(socket_fd, read_buffer, MAX_MSG_SIZE, 0);
+	if(return_call < 0){
+    	printf("Error reading encrypted key\n");
+    	return -1;
+    }
+
+    // Determine end of message and store in string
+    read_buffer[return_call] = 0;
+    std::string encrypted_alice_key_half(read_buffer);
+    encrypted_alice_key_half = hexstr2str(encrypted_alice_key_half);
+
+    // Decrypt Alice's half of the key
+    std::string alice_key_half = decryptWithBothKeys("alice.pub", "bob.key", encrypted_alice_key_half);
+
+    printf("Key received from Alice. Key agreed\n");
+
+    // Determine session key and split it into aes_key, aes_iv, hmac_password, and init_nonce
+    std::string session_key = session_hash(bob_key_half, alice_key_half);
+    std::string aes_key = hexstr2str(session_key.substr(0,32));
+    std::string aes_iv = hexstr2str(session_key.substr(32,32));
+    std::string hmac_password = session_key.substr(64,32);
+    std::string init_nonce = session_key.substr(96,32);
+
+    printf("Here is the nonce: %s\n", init_nonce.c_str());
 
 	return 0;
 }
