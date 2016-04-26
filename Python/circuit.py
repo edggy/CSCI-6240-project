7a659d879f17820ed3db233947d73eecc6e6f1af
edgchess <edgchess@aol.com>
2016-04-26 17:19:59 -0400

    Got the greater than circuit to work

--- Python/circuit.py
+++ Python/circuit.py
@@ -1,60 +1,59 @@
-class Gate:
-	def __init__(self, name, table, parents = (), children = set()):
+class BinaryGate:
+	'''
+	A binary gate has exactly two inputs and one output
+	'''
+
+	def __init__(self, name, table):
 		'''
-		@param name - The name for this gate
-		@param table - A dict of all the valid inputs to their outputs
-		@param parents - A tuple of all of the input edges
-		@param child - A set of all of the output edges
+		@param name - The unique identifier for this gate
+		@param table - A dict of input tuples of length 2 to output truth values
+		@param parents - A tuple of length 2 containing the incoming edges
+		@param child - The outgoing edge
 		'''
 		self.name = name
-		self._table = table
-		self._parents = parents
-		self._children = children		
-	
+		self.table = table
+		self.parents = set([])
+		self.child = None
+
+	def __hash__(self):
+		return hash(self.name)
+
 	def __str__(self):
-		ret = str(self.name)
-		for row in self._table:
-			rowStr = ''
-			for i in row:
-				rowStr += str(i) + '\t'
-			rowStr += self._table[row]
-			ret += '\n' + rowStr
-		
-		return ret
-	
+		return self.name
+
 	def __repr__(self):
-		return '%s:\t%s\t%s\t%s' % (self.name, repr(self._table), repr(self._parents), repr(self._children)) 
-	
+		return self.name
+		#ret = self.name
+		#for row in self.table:
+			#ret += '\n%s\t%s\t%s' % (row[0], row[1], self.table[row])
+		#return ret + '\n' + str(self.parents) + '\n' + str(self.child)
+
+	def eval(self, input1, input2):
+		return self.table[(input1, input2)]
+
 	def __call__(self, *args):
-		if args not in self._table:
-			return None
-		return self._table[args]
-	
-	def setChildren(self, *args):
-		output = self(*args)
-		for child in self._children:
-			child.value = output
-	
+		if len(args) == 1:
+			return self.eval(args[0], args[0])		
+		if len(args) == 2:
+			return self.eval(args[0], args[1])
+
 	def garble(self, n):
 		'''
 		@param n - the size of the key in bytes
 		@return - a garbled gate of this gate
 		'''
-		
-		# a dict from output edge names to a set of (encryped value, (MAC, key)) to be used to get the result dict<edge,set<tup<enc,tup<mac,key>>>>
-		encrypedTable = {}
-		
-		for args in self._table:
+		encrypedTable = []
+		for args in self.table:
 			# args is the current tuple of arguments
-			
+
 			# encArgs is a list of the encrypted values
 			encArgs = []
 			for i, arg in enumerate(args):
 				# i is the argument number, arg is the argument value
-				
+
 				# get the ith parent's arg value
-				encArgs.append(self._parents[i].value[arg])
-			
+				encArgs.append(self.parents[i].value[arg])
+
 			# key is the encryption of all inputs
 			key = None
 			for arg in encArgs:
@@ -62,105 +61,58 @@ class Gate:
 					key = arg
 				else:
 					key = pad.encrypt(key, n, arg)[0]
-			
+
 			# outVal is the value of the output
-			outVal = self._table[args]
-			
-			# Compute the table for each child edge seperately
-			for child in self._children:
-				
-				# The value is the value of the output edge
-				value = child.value[outVal]
-				
-				# The encrypted value is the encryption of the value
-				encVal = str(pad.encrypt(value, n, key)[0])
-				
-				# Generate a random MAC key
-				MACkey = os.urandom(n*2)
-				
-				# Compute the MAC
-				mac = str(pad.mac(value, n, MACkey)[0])
-				
-				# Add this edge to the encrypedTable
-				encrypedTable[child.name] = (encVal, (MAC, MACkey))
-			
-		return GarbledGate(encrypedTable, n)
-	
-class GarbledGate(Gate):
-	def __init__(self, encrypedTable, n):
-		'''
-		@param table - a dict from output edge names to a set of (encryped value, (MAC, key)) to be used to get the result dict<edge,set<tup<enc,tup<mac,key>>>>
-		@param n - the size of the key in bytes
-		'''
-		Gate.__init__(self, encrypedTable)
-		self.n = n
-		
-	def __call__(self, *args):
-		import otp
-		pad = otp.OTP()
-		
-		# key is the encryption of all inputs
-		key = None
-		for arg in reversed(args):
-			if key is None:
-				key = arg
-			else:
-				key = pad.decrypt(key, self.n, arg)[0]
-		
-		# ret is a dict from child edge names to their values
-		ret = {}
-		
-		for child in self._children:
-			result = None
-			# Try each encrypted value
-			for encVal, MACdata in self._table[child.name]:
-				# Get the value of the decryption
-				value = pad.decrypt(encVal, self.n, key)[0]
-				
-				# Use the MACdata to check if the decryption is valid
-				if pad.verify(value, MACdata[0], self.n, MACdata[1])[0]:
-					# If there is a hit, no need to continue
-					result = str(value)
-					break
-		
-			# If we got no result something went wrong...
-			if not result:
-				raise Exception
-			
-			ret[child.name] = result
-		
-		return result
-	
-	def setChildren(self, *args):
-		output = self(*args)
-		for child in self._children:
-			child.value = output[child.name]
-		
+			outVal = self.table[args]     
+
+			# The value is the value of the output edge
+			value = self.child.value[outVal]
+
+			# The encrypted value is the encryption of the value
+			encVal = str(pad.encrypt(value, n, key)[0])
+
+			## Generate a random MAC key
+			#MACkey = os.urandom(n*2)
+
+			## Compute the MAC
+			#mac = str(pad.mac(value, n, MACkey)[0])
+
+			# Add this edge to the encrypedTable
+			encrypedTable.append(encVal)
+
+		return GarbledBinaryGate(self.name, encrypedTable, self.parents, self.child)          
+
+class GarbledBinaryGate(BinaryGate):
+	pass
 
 class Edge:
 	'''
 	An edge connecting the output of one gate to the input of another
 	'''
-	def __init__(self, name, child = None, parent = None, value = None):
+	def __init__(self, name, parent = None, child = None, value = None):
 		'''
 		@param name - The name of the edge
-		@param child - The gate that this feeds into
-		@param parent - The gate that this came from
+		@param child - The name of the gate that this feeds into
+		@param parent - The name of the gate that this came from
 		@param value - The value of this edge
 		'''
 		self.name = name
-		self._parent = parent
-		self._child = child
-		self._value = value
-		
+		self.parent = parent
+		self.child = child
+		self.value = value
+
 	def __str__(self):
-		return '%s:\t%s\t%s\t%s' % (self.name, self._parent.name, self._child.name, self._value)
-	
+		return '%s:\t%s\t%s\t%s' % (self.name, self.parent, self.child, self.value)
+
 	def __repr__(self):
-		return '%s:\t%s\t%s\t%s' % (self.name, self._parent.name, self._child.name, self._value)
+		return '%s:\t%s\t%s\t%s' % (self.name, self.parent, self.child, self.value)
 	
+	def __hash__(self):
+		return hash(self.name)
+
 	def garble(self, n):
-		return GarbledEdge(self.name, self._child, self._parent, {0:os.urandom(n), 1:os.urandom(n)})
+		# Randomly assign values to represent each state
+		return GarbledEdge(self.name, self.child, self.parent, {0:os.urandom(n), 1:os.urandom(n)})
 
 class GarbledEdge(Edge):
 	'''
@@ -173,4 +125,153 @@ class Circuit:
 		'''
 		@param gates - A dict of IDs to gates
 		@param edges - A dict of IDs to edge
-		'''        
+		'''
+		self.gates = gates
+		self.edges = edges
+		
+		lastEdge = None
+		for edgeID in self.edges:
+			edge = self.edges[edgeID]
+			
+			if edge.parent is not None:
+				self.gates[edge.parent].child = edgeID
+			if edge.child is not None:
+				print self.gates[edge.child].parents
+				self.gates[edge.child].parents.add(edgeID)
+			if lastEdge:
+				print lastEdge is edge
+			lastEdge = edge
+				
+	def evaluate(self, inputs, outputs):
+		'''
+		@param inputs - A map of ID to their initial states
+		@param outputs - A set of ID of outputs
+		'''
+		from collections import deque
+		result = {}
+	
+		queue = deque()
+		
+		# set the input edges to their input values
+		for edgeID in inputs:
+			edgeValue = inputs[edgeID]
+			self.edges[edgeID].value = edgeValue
+			queue.appendleft(edgeID)
+			
+		while len(queue) > 0:
+			edgeID = queue.pop()
+			edge = self.edges[edgeID]
+			
+			if edgeID in outputs:
+				# If the edge is in outputs, add it to results
+				result[edgeID] = edge.value
+		
+			if edge.child is not None:
+				# If we have a child
+				vals = []
+				child = self.gates[edge.child]
+				
+				# Gather each of the gate's parent's values
+				for parentEdgeID in child.parents:
+					parent = self.edges[parentEdgeID]
+					vals.append(parent.value)
+					
+				# Check that each of the gate's parent has a value
+				if None not in vals:
+					# Evaluate the gate with the arguments
+					value = child(*vals)
+					edgeChild = self.edges[child.child]
+					edgeChild.value = value
+					queue.appendleft(child.child)
+		
+		return result		
+			
+			
+	
+class GarbledCircuit(Circuit):
+	pass
+
+
+if __name__ == '__main__':
+	ifTable = {(1,1):1,(1,0):0, (0,1):1, (0,0):1}
+	nifTable = {(1,1):0,(1,0):1, (0,1):0, (0,0):0}
+	iffTable = {(1,1):1,(1,0):0, (0,1):0, (0,0):1}
+	not1Table = {(1,1):0,(1,0):0, (0,1):1, (0,0):1}
+	andTable = {(1,1):1,(1,0):0, (0,1):0, (0,0):0}
+	orTable = {(1,1):1,(1,0):1, (0,1):1, (0,0):0}
+	#nif = and(a,not(b)) = 0, 1, 0, 0
+	n = 4
+	
+	def makeGTpart(n):
+		gates = {}
+		edges = {}		
+		
+		
+		needsAnding = []
+		
+		#needsAnding.append('a%02d%02d' % (n,n))
+		
+		notGate = BinaryGate('not b%02d%02d' % (n,n), not1Table)
+		edges['b%02d%02d' % (n,n)] = Edge('b%02d%02d' % (n,n), None, 'not b%02d%02d' % (n,n))
+		gates[notGate.name] = notGate
+		
+		andGate = BinaryGate('and %s %s' % ('a%02d%02d' % (n,n),notGate), andTable)
+		gates[andGate.name] = andGate
+		edges['a%02d%02d' % (n,n)] = Edge('a%02d%02d' % (n,n), None, andGate.name)
+		edges[notGate.name] = Edge(notGate.name, notGate.name, andGate.name)
+		needsAnding.append(andGate.name)
+		
+		for i in range(n):
+			iffGate = BinaryGate('iff a%02d%02d b%02d%02d' % (i, n, i, n), iffTable)
+			edges['a%02d%02d' % (i,n)] = Edge('a%02d%02d' % (i,n), None, 'iff a%02d%02d b%02d%02d' % (i, n, i, n))
+			edges['b%02d%02d' % (i,n)] = Edge('b%02d%02d' % (i,n), None, 'iff a%02d%02d b%02d%02d' % (i, n, i, n))
+			needsAnding.append(iffGate.name)
+			gates[iffGate.name] = iffGate		
+			# TODO
+		
+		#if n == 0:
+			#edges['a%02d%02d' % (n,n)] = Edge('a00,00', 'a00,00', 'and: not: b00,00 a00,00')
+			#needsAnding.append(edges['a00,00'])
+			
+		lastAnd = None
+		while len(needsAnding) > 0:
+			nextGate = needsAnding.pop()
+			if lastAnd is None:
+				lastAnd = nextGate
+			else:
+				andGate = BinaryGate('and %s %s' % (lastAnd,nextGate), andTable)
+				edges[lastAnd] = Edge(lastAnd, lastAnd, andGate.name)
+				edges[nextGate] = Edge(nextGate, nextGate, andGate.name)
+				gates[andGate.name] = andGate
+				lastAnd = andGate.name
+		
+		return (gates, edges, lastAnd)
+	
+	gates, edges, lastOutput = makeGTpart(0)
+	for i in range(1, n):
+		curGates, curEdges, output = makeGTpart(i)
+		for g in curGates:
+			gates[g] = curGates[g]
+		#print curEdges
+		for e in curEdges:
+			edges[e] = curEdges[e]
+		
+		orGate = BinaryGate('or %s %s' % (lastOutput,output), orTable)
+		edges[lastOutput] = Edge(lastOutput, lastOutput, orGate.name)
+		edges[output] = Edge(output, output, orGate.name)
+		gates[orGate.name] = orGate
+		lastOutput = orGate.name
+	edges['output'] = Edge('output', lastOutput)
+			
+	
+	
+	c = Circuit(gates, edges)
+	inputs = {}
+	for i in range(n):
+		a = {0:0, 1:1, 2:1, 3:0}
+		b = {0:0, 1:1, 2:0, 3:0}
+		for j in range(n):
+			if i <= j:
+				inputs['a%02d%02d' % (i,j)] = a[i]
+				inputs['b%02d%02d' % (i,j)] = b[i]
+	print c.evaluate(inputs, ('output',))
